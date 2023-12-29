866baf2c8762a2efa0ee1f13edd542f53ffa5ddf
ManobhavSachan <manobhavsachan@gmail.com>
2023-12-29 23:41:12 +0530

    Chart Logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,205 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import SelectMenu from "./components/SelectMenu";
 import BarChart from "./components/BarChart";
+import axios from "axios";
+import "./index.css";
 
 function App() {
-  const data = {
-    data: [
-      { letter: "A", frequency: 0.08167 },
-      { letter: "B", frequency: 0.01492 },
-      { letter: "C", frequency: 0.02782 },
-      { letter: "D", frequency: 0.04253 },
-      { letter: "E", frequency: 0.12702 },
-      { letter: "F", frequency: 0.02288 },
-      { letter: "G", frequency: 0.02015 },
-      { letter: "H", frequency: 0.06094 },
-      { letter: "I", frequency: 0.06966 },
-      { letter: "J", frequency: 0.00153 },
-      { letter: "K", frequency: 0.00772 },
-      { letter: "L", frequency: 0.04025 },
-      { letter: "M", frequency: 0.02406 },
-      { letter: "N", frequency: 0.06749 },
-      { letter: "O", frequency: 0.07507 },
-      { letter: "P", frequency: 0.01929 },
-      { letter: "Q", frequency: 0.00095 },
-      { letter: "R", frequency: 0.05987 },
-      { letter: "S", frequency: 0.06327 },
-      { letter: "T", frequency: 0.09056 },
-      { letter: "U", frequency: 0.02758 },
-      { letter: "V", frequency: 0.00978 },
-      { letter: "W", frequency: 0.0236 },
-      { letter: "X", frequency: 0.0015 },
-      { letter: "Y", frequency: 0.01974 },
-      { letter: "Z", frequency: 0.00074 }
-    ],
-    columns: ["letter", "frequency"]
+  // const data = {
+  //   data: [
+  //     { letter: "A", frequency: 0.08167 },
+  //     { letter: "B", frequency: 0.01492 },
+  //     { letter: "C", frequency: 0.02782 },
+  //     { letter: "D", frequency: 0.04253 },
+  //     { letter: "E", frequency: 0.12702 },
+  //     { letter: "F", frequency: 0.02288 },
+  //     { letter: "G", frequency: 0.02015 },
+  //     { letter: "H", frequency: 0.06094 },
+  //     { letter: "I", frequency: 0.06966 },
+  //     { letter: "J", frequency: 0.00153 },
+  //     { letter: "K", frequency: 0.00772 },
+  //     { letter: "L", frequency: 0.04025 },
+  //     { letter: "M", frequency: 0.02406 },
+  //     { letter: "N", frequency: 0.06749 },
+  //     { letter: "O", frequency: 0.07507 },
+  //     { letter: "P", frequency: 0.01929 },
+  //     { letter: "Q", frequency: 0.00095 },
+  //     { letter: "R", frequency: 0.05987 },
+  //     { letter: "S", frequency: 0.06327 },
+  //     { letter: "T", frequency: 0.09056 },
+  //     { letter: "U", frequency: 0.02758 },
+  //     { letter: "V", frequency: 0.00978 },
+  //     { letter: "W", frequency: 0.0236 },
+  //     { letter: "X", frequency: 0.0015 },
+  //     { letter: "Y", frequency: 0.01974 },
+  //     { letter: "Z", frequency: 0.00074 },
+  //   ],
+  //   // columns: ["letter", "frequency"],
+  // };
+  const [intensity, setIntensity] = useState({});
+  const [likelihood, setLikelihood] = useState({});
+  const [relevance, setRelevance] = useState({});
+  const [year, setYear] = useState({});
+  const [country, setCountry] = useState({});
+  const [topics, setTopics] = useState({});
+  const [region, setRegion] = useState({});
+  const [city, setCity] = useState({});
+  const [apiData, setApiData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post(
+          "https://backend-black-coffers.onrender.com/api/data",
+          {
+            // Your filter parameters go here
+            // end_ear: 2023,
+            // region: "Western Africa",
+            // Add other parameters as needed
+          }
+        );
+
+        setApiData(response.data);
+        setIntensity(createDataObject(response.data, "intensity"));
+        setLikelihood(createDataObject(response.data, "likelihood"));
+        setRelevance(createDataObject(response.data, "relevance"));
+        setYear(createDataObject(response.data, "end_year"));
+        setCountry(createDataObject(response.data, "country"));
+        setTopics(createDataObject(response.data, "topic"));
+        setRegion(createDataObject(response.data, "region"));
+        setCity(createDataObject(response.data, "city"));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setError("Error fetching data");
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array to execute the effect only once when the component mounts
+
+  const createDataObject = (responseData, property) => {
+    // Create an array to store the data
+    const data = [];
+
+    // Iterate through the response data
+    responseData.forEach((item) => {
+      const propertyValue = item[property];
+
+      const existingDataObject = data.find(
+        (obj) => obj[property] === propertyValue
+      );
+
+      if (existingDataObject) {
+        // If the data object is present, update its frequency value
+        existingDataObject.frequency++;
+      } else {
+        // If the data object is not present, create a new one
+        data.push({
+          [property]: propertyValue,
+          frequency: 1, // Set the initial frequency to 1
+        });
+      }
+    });
+    data.sort((a, b) => a[property] - b[property]);
+    console.log("ans", { data });
+    // setTest({data});
+    return { data };
   };
-  
-  const [count, setCount] = useState(0);
 
   return (
     <>
       <Navbar />
       <div className="p-4">
-      <div className="flex justify-center space-x-4">
-      <SelectMenu title = "End Year"/>
-      <SelectMenu title = "Topic"/>
-      <SelectMenu title = "Sector"/>
-      <SelectMenu title = "Region"/>
-      
+        <div className="flex justify-center space-x-4">
+          <SelectMenu title="End Year" />
+          <SelectMenu title="Topic" />
+          <SelectMenu title="Sector" />
+          <SelectMenu title="Region" />
+        </div>
+        <div className="flex justify-center space-x-4 pt-2">
+          <SelectMenu title="Pestle" />
+          <SelectMenu title="Source" />
+          <SelectMenu title="Country" />
+        </div>
+        <div className="flex justify-center space-x-4 pt-2">
+          
+          {likelihood.data && (
+            <BarChart
+              data={likelihood}
+              xlabel="Likelihood"
+              ylabel="Frequency"
+              keyword="likelihood"
+              rotate={false}
+            />
+          )}
+          {relevance.data && (
+            <BarChart
+              data={relevance}
+              xlabel="Relevance"
+              ylabel="Frequency"
+              keyword="relevance"
+            />
+          )}
+        </div>
+        <div className="flex justify-center space-x-4 pt-2">
+        {intensity.data && (
+            <BarChart
+              data={intensity}
+              xlabel="Intensity"
+              ylabel="Frequency"
+              keyword="intensity"
+            />
+          )}
+          {year.data && (
+            <BarChart
+              data={year}
+              xlabel="Year"
+              ylabel="Frequency"
+              keyword="end_year"
+            />
+          )}
+          
+        </div>
+        <div className="flex justify-center space-x-4 pt-2">
+        {country.data && (
+            <BarChart
+              data={country}
+              xlabel="Country"
+              ylabel="Frequency"
+              keyword="country"
+              rotate={true}
+            />
+          )}
+          </div>
+          <div className="flex justify-center space-x-4 pt-2">
+          {topics.data && (
+            <BarChart
+              data={topics}
+              xlabel="Topics"
+              ylabel="Frequency"
+              keyword="topic"
+              rotate={true}
+            />
+          )}
+          </div>
+        <div className="flex justify-center space-x-4 pt-2">
+          {region.data && (
+            <BarChart
+              data={region}
+              xlabel="Region"
+              ylabel="Frequency"
+              keyword="region"
+              rotate={true}
+            />
+          )}
+        </div>
       </div>
-      <div className="flex justify-center space-x-4 pt-2">
-      
-      <SelectMenu title = "Pestle"/>
-      <SelectMenu title = "Source"/>
-      <SelectMenu title = "Country"/>
-      </div>
-      <BarChart data = {data}/>
-      </div>
-      
+
       {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
     </>
   );
--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,70 +1,158 @@
-import React, { useEffect, useRef } from 'react';
-import * as d3 from 'd3';
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
+import "./../index.css";
 
-const BarChart = ({ data }) => {
-    console.log(data);
-    const chartRef = useRef();
+const BarChart = (props) => {
+  const data = props.data;
+  console.log("bar", data);
+  const letter = props?.keyword || "letter";
+  const chartRef = useRef();
 
   useEffect(() => {
-    const width = 928;
+    const width = 900;
     const height = 500;
-    const marginTop = 20;
+    const marginTop = 40; // Increased margin for title
     const marginRight = 0;
-    const marginBottom = 30;
-    const marginLeft = 40;
+    const marginBottom = 100; // Increased margin for x-axis label and scrollable container
+    const marginLeft = 70; // Increased margin for y-axis label
 
-    const x = d3.scaleBand()
-      .domain(d3.sort(data.data, d => -d.frequency).map(d => d.letter))
+    // Tooltip
+    const tooltip = d3
+      .select("body")
+      .append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
+
+    const x = d3
+      .scaleBand()
+      .domain(data.data.map((d) => d[letter]))
       .range([marginLeft, width - marginRight])
       .padding(0.1);
 
     const xAxis = d3.axisBottom(x).tickSizeOuter(0);
 
-    const y = d3.scaleLinear()
-      .domain([0, d3.max(data.data, d => d.frequency)]).nice()
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(data.data, (d) => d.frequency)])
+      .nice()
       .range([height - marginBottom, marginTop]);
 
-    const svg = d3.select(chartRef.current)
+    const svg = d3
+      .select(chartRef.current)
       .attr("viewBox", [0, 0, width, height])
       .attr("width", width)
       .attr("height", height)
       .attr("style", "max-width: 100%; height: auto;")
       .call(zoom);
 
-    svg.append("g")
+    svg
+      .append("g")
       .attr("class", "bars")
-      .attr("fill", "steelblue")
+      .attr("fill", "#6366f1")
       .selectAll("rect")
       .data(data.data)
       .join("rect")
-      .attr("x", d => x(d.letter))
-      .attr("y", d => y(d.frequency))
-      .attr("height", d => y(0) - y(d.frequency))
-      .attr("width", x.bandwidth());
+      .attr("x", (d) => x(d[letter]))
+      .attr("y", (d) => y(d.frequency))
+      .attr("height", (d) => y(0) - y(d.frequency))
+      .attr("width", x.bandwidth())
+      .on("mouseover", (event, d) => {
+        const xValue = d[letter];
+        const yValue = d.frequency;
+        // console.log("x", xValue, "y", yValue);
+        tooltip.transition().duration(200).style("opacity", 0.9);
 
-    svg.append("g")
-      .attr("class", "x-axis")
-      .attr("transform", `translate(0,${height - marginBottom})`)
-      .call(xAxis);
+        tooltip
+          .html(`<strong>${xValue}</strong><br/>Frequency: ${yValue}`)
+          .style("left", event.pageX + 5 + "px")
+          .style("top", event.pageY - 28 + "px");
+      })
+      .on("mouseout", () => {
+        tooltip.transition().duration(500).style("opacity", 0);
+      });
 
-    svg.append("g")
-      .attr("class", "y-axis")
-      .attr("transform", `translate(${marginLeft},0)`)
-      .call(d3.axisLeft(y))
-      .call(g => g.select(".domain").remove());
+    // X Axis
+    if (props.rotate) {
+      svg
+        .append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0,${height - marginBottom})`)
+        .call(xAxis)
+        .selectAll("text")
+        .attr("y", 0)
+        .attr("x", -9)
+        .attr("dy", ".35em")
+        .attr("transform", "rotate(-90)")
+        .style("text-anchor", "end");
+    } else {
+      svg
+        .append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0,${height - marginBottom})`)
+        .call(xAxis);
+    }
+
+    // Title
+    svg
+      .append("text")
+      .attr("class", "chart-title")
+      .attr("x", width / 2)
+      .attr("y", marginTop / 2)
+      .style("text-anchor", "middle")
+      .style("font-size", "20px")
+      .text(props.title);
+
+    // X Axis Label
+    svg
+      .append("text")
+      .attr("class", "x-axis-label")
+      .attr("x", width / 2)
+      .attr("y", height - marginBottom / 3) // Adjusted position for better spacing
+      .style("text-anchor", "middle")
+      .text(props.xlabel);
+
+    // Y Axis Label
+    // Y Axis
+svg
+.append("g")
+.attr("class", "y-axis")
+.attr("transform", `translate(${marginLeft},0)`)
+.call(d3.axisLeft(y))
+.call(g => g.select(".domain").remove());
+    svg
+      .append("text")
+      .attr("class", "y-axis-label")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -height / 2)
+      .attr("y", marginLeft / 3) // Adjusted position for better spacing
+      .style("text-anchor", "middle")
+      .text(props.ylabel);
 
     function zoom(svg) {
-      const extent = [[marginLeft, marginTop], [width - marginRight, height - marginTop]];
+      const extent = [
+        [marginLeft, marginTop],
+        [width - marginRight, height - marginTop],
+      ];
 
-      svg.call(d3.zoom()
-        .scaleExtent([1, 8])
-        .translateExtent(extent)
-        .extent(extent)
-        .on("zoom", zoomed));
+      svg.call(
+        d3
+          .zoom()
+          .scaleExtent([1, 8])
+          .translateExtent(extent)
+          .extent(extent)
+          .on("zoom", zoomed)
+      );
 
       function zoomed(event) {
-        x.range([marginLeft, width - marginRight].map(d => event.transform.applyX(d)));
-        svg.selectAll(".bars rect").attr("x", d => x(d.letter)).attr("width", x.bandwidth());
+        x.range(
+          [marginLeft, width - marginRight].map((d) =>
+            event.transform.applyX(d)
+          )
+        );
+        svg
+          .selectAll(".bars rect")
+          .attr("x", (d) => x(d[letter]))
+          .attr("width", x.bandwidth());
         svg.selectAll(".x-axis").call(xAxis);
       }
     }
